7561a3627b8d86bde6508ea282e02e00093f963d
jhon3242 <78288539+jhon3242@users.noreply.github.com>
2021-02-18 14:08:59 +0900

    Add files via upload

--- play5.js
+++ play5.js
@@ -1,9 +1,18 @@
-
 const body= document.querySelector("body");
 const count = document.querySelector("#count");
+const form = document.querySelector(".new");
+const input = form.querySelector("input");
 const box=[
     "Alpha","Bravo","Chalie","Delta"
 ]
+
+//새로운 정보 업로드
+function upload(event){
+    event.preventDefault();
+    const input = form.input;
+    box.push(input);
+    console.log("hi");
+}
 
 
 
@@ -29,8 +38,10 @@ function writeCount(){
 }
 
 function init(){
+    
     writeBox();
     writeCount();
+    form.addEventListener("submit", upload);
 }
 
 init ();
